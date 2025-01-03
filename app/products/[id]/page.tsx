f367a891cad8d3cd7bb6b2feda1406c1550f8da3
“sidra <fsidra167@gmail.com>
2025-01-03 15:39:42 +0500

    commit

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,28 +1,39 @@
-import Link from "next/link";
-import products from "@/data/ltsblog"
+"use client"
+import { useRouter } from "next/navigation";
+import CommentSection from "@/components/comments"; 
 import Image from "next/image";
+import allblogs from "@/data/blog";
 
+const ProductDetailPage = ({ params }: { params: { id: string } }) => {
+    console.log(params)
+    const route = useRouter();
+    const blog = allblogs.find((b) => b.id === params.id)
 
-const ProductsPage = () => {
+    if (!blog) {
+        return <p>Product not found!</p>;
+    }
+
     return (
-        <div>
-            <h1>Our Products</h1>
-            <div className="grid grid-cols-2 gap-4">
-                {products.map((product) => (
-                    <div key={product.id} className="border p-4 rounded-md shadow-md">
-                        <Image src={product.image} alt={product.name} height={400}width={400} className="w-full h-48 object-cover" />
-                        <h2 className="text-lg font-bold">{product.name}</h2>
-                        <p className="text-lg">{product.description}</p>
-                        <Link href={`/products/${product.id}`}>
-                            <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">
-                                View 
-                            </button>
-                        </Link>
-                    </div>
-                ))}
+        <div className="bg-zinc-200 min-h-screen">
+            <h1 className="text-5xl font-bold text-black mx-5 py-6">{blog.name}</h1>
+            <Image src={blog.image} alt={blog.name} width={500} height={500} className="w-full h-80 object-cover my-4" />
+            <p className="text-lg">{blog.description}</p>
+            <br />
+            <p className="text-lg">{blog.description2}</p>
+            <br />
+            <p className="text-lg">{blog.description3}</p>
+            <br />
+            <br />
+            <p className="text-lg">{blog.description4}</p>
+            <CommentSection />
+            
+            <div className='flex items-center justify-center bg-zinc-300 py-10'>
+                <button onClick={() => route.push("/")} className='mt- bg-black hover:bg-fuchsia-500 text-zinc-800 font-semibold px-4 py-2 rounded border border-zinc-500'>
+                    Go Back
+                </button>
             </div>
         </div>
     );
 };
 
-export default ProductsPage
+export default ProductDetailPage;
